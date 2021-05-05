de46ab1a5e73d2a75f70ea6d0baddadcc257ed1d
LucasGuimel <guimellucas@gmail.com>
2021-05-05 10:00:41 -0300

    iniciando a aplicacao front-end

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Login from './views/login'
+
+import 'bootswatch/dist/flatly/bootstrao.css'
+import './custon.css'
+
+class App extends React.Component {
+  render(){
+    return(
+    <div>
+      <Login />
+    </div>
+    )
+  }
+}
+
+export default App
